930329eb9fc11e21df381d8589a270484da150bc
Feuerhamster <feuerhamster02@gmail.com>
2021-01-05 20:20:14 +0100

    Added comment

--- utils/queryNumberParser.js
+++ utils/queryNumberParser.js
@@ -1,3 +1,6 @@
+/**
+ * Express.js middleware to parse numbers in the querystring of a request to an actual number (float)
+ */
 function queryNumberParser(req, res, next) {
 
     for(let [key, value] of Object.entries(req.query)) {
