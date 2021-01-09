73764822551a58b1803bc1105af28d08910d5721
ElectronicBlueberry <44241786+ElectronicBlueberry@users.noreply.github.com>
2021-01-10 00:17:12 +0100

    remvoed duplicate interfaces

--- source/models/user.ts
+++ source/models/user.ts
@@ -1,14 +1,3 @@
-import MongoDB from "mongodb";
-
-
-/** Body of create user post request */
-export interface CreateUser {
-    username: string,
-    email: string,
-    /** Whether or not this user is an admin */
-    admin: boolean
-}
-
 /** validate.js create user post request validation schema */
 export const createUser = {
 
@@ -34,13 +23,6 @@ export const createUser = {
 
 }
 
-/** Body of login post request */
-export interface LoginBody {
-    /** username or email */
-    username: string,
-    password: string,
-}
-
 /** validate.js user login post request validation schema */
 export const loginBody = {
 
@@ -54,12 +36,6 @@ export const loginBody = {
         type: "string"
     }
 
-}
-
-/** Body of update password post request */
-export interface UpdatePassword {
-    old?: string,
-    new: string
 }
 
 /** validate.js password post request validation schema */
@@ -84,11 +60,6 @@ export const updatePassword = {
 
 }
 
-/** Body of reset email post request */
-export interface ResetEmail {
-    email: string
-}
-
 /** validate.js reset email post request validation schema */
 export const resetEmail = {
 
@@ -98,11 +69,6 @@ export const resetEmail = {
         email: true
     }
 
-}
-
-/** Body of reset username post request */
-export interface ResetUsername {
-    username: string
 }
 
 /** validate.js reset username post request validation schema */
@@ -117,34 +83,4 @@ export const resetUsername = {
         }
     }
 
-}
-
-/** User object as stored in database */
-export interface User {
-    _id: string | number | MongoDB.ObjectID,
-    username: string,
-    email: string,
-    password: Password,
-    registerDate: Date,
-    lastLogin: null | Date,
-    admin: boolean
-}
-
-/** User object as sent in get requests */
-export type ResUser = {
-    password: never
-} & Omit<User, keyof {password}>
-
-/** Just created user, with generated plaintext password */
-export type NewApiUser = {
-    password: string,
-} & Omit<User, keyof {password}>
-
-/** User object for new database entries */
-export type NewDbUser = Omit<User, keyof{_id}>
-
-/** Encrypted password with salt, as stored in database */
-export interface Password {
-    key: string,
-    salt: string
 }
