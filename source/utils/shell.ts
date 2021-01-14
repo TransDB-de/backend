522b9d2a7b3ac2f8832c0c91ceb129963b042594
ElectronicBlueberry <44241786+ElectronicBlueberry@users.noreply.github.com>
2021-01-14 20:33:04 +0100

    new shell ultitly executes shell commands in a safe manner

---
+++ source/utils/shell.ts
@@ -0,0 +1,83 @@
+import { exec } from "child_process";
+import { promisify } from "util";
+
+// Helper module to execute shell commands, from a set of commands
+// and to check if all commands are installed
+
+// convert exec to async
+const asyncExec = promisify(exec);
+
+/**
+ * All avalible commands
+ */
+const commands = {
+    exportEntries: {
+        getCommand: (args: string[]) => `mongoexport --uri "${args[0]}" --db ${args[1]} --out "${args[2]}" --collection entries --jsonArray --quiet`,
+        getTest: () => `mongoexport --version`
+    }
+}
+
+/**
+ * Export entries collection to a gzipped json file
+ * @param dbUri uri to database to export entries from
+ * @param outFile path to save file to
+ * @returns sucess
+ */
+export async function exportEntries(dbUri: string, dbName: string, outFile: string): Promise<boolean> {
+    let [sucess] = await runCommand("exportEntries", dbUri, dbName, outFile);
+    return sucess;
+}
+
+/**
+ * Exectues a command
+ * @returns Tuple: [ did command succeed, command output ]
+ */
+async function runCommand(command: keyof typeof commands, ...stringArgs: string[]): Promise<[boolean, string]> {
+
+    try {
+
+        let { stdout, stderr } = await asyncExec(
+            commands[command].getCommand( stringArgs )
+        );
+
+        if (stderr) {
+            return [ false, stderr ];
+        } else {
+            return [ true, stdout ];
+        }
+
+    } catch(e) {
+
+        return [ false, e ];
+
+    }
+
+}
+
+/**
+ * Tests if all commands are installed on this machine
+ */
+export async function testForCommands(): Promise<void> {
+    let command = "";
+
+    try {
+        
+        for (let [key, val] of Object.entries(commands) ) {
+
+            command = val.getTest();
+            let { stderr } = await asyncExec( command );
+
+            if (stderr) {
+                throw(stderr);
+            }
+
+        }
+
+    } catch(e) {
+
+        console.log(`Command "${command}" failed to run.`);
+        console.log(`Check if the required command is installed on this machine!`);
+        console.error(e);
+
+    }
+}
